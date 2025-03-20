3a61debeec07b38c6abdcec1c710ebf9670da7f6
David Gomez Camargo <david.gomez2008@outlook.de>
2025-03-20 21:23:57 +0100

    Adding Java Files

---
+++ MeinInfoKurs/DE/Files/JavaEditor/Schuelersprecher.java
@@ -0,0 +1,22 @@
+/**
+*
+* Keine Garantie auf vollständigkeit oder funktionsfähigkeit. 
+*
+* @version 1.0 vom 20.03.2025
+* @MeinInfoKurs.github.io
+*/
+public class Schuelersprecher {
+  
+  public static void main(String[] args) {
+    String Kandidat1="", Kandidat2="", Kandidat3="";
+    int StimmenK1=0, StimmenK2=0, StimmenK3=0, GesamtStimmen=0;
+    double Prozent1=0, Prozent2=0, Prozent3=0;
+    
+    Kandidat1 = In.out.readString("Wie heißt der erste Kandidat?: ");
+    Kandidat2 = In.out.readString("Wie heißt der zweite Kandidat?: ");
+    Kandidat3 = In.out.readString("Wie heißt der dritte Kandidat?: ");
+    
+    System.out.println(Kandidat1 "+" Kandidat2 "+" Kandidat3);
+  } // end of main
+  
+} // end of class Schuelersprecher 
