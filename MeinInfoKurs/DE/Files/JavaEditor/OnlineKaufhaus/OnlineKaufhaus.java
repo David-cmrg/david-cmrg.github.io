e36abe74a63a9dca6f0b4bcf28372410380010ff
David Gomez Camargo <david.gomez2008@outlook.de>
2025-03-20 21:21:52 +0100

    Adding Java Files

---
+++ MeinInfoKurs/DE/Files/JavaEditor/OnlineKaufhaus/OnlineKaufhaus.java
@@ -0,0 +1,36 @@
+/**
+*
+* Keine Garantie auf vollständigkeit oder funktionsfähigkeit. 
+*
+* @version 1.0 vom 20.03.2025
+* @MeinInfoKurs.github.io
+*/
+
+public class OnlineKaufhaus {
+  
+  public static void main(String[] args) {
+
+    double Versandpauschale;
+    double Rabatt;
+    int Preis;
+    int Auftragswert;
+    String Ausgabe;
+
+    Versandpauschale=3.5;
+    Rabatt=0.02;
+    Preis=100;
+    Auftragswert=0;
+    Auftragswert="Wie_hoch_belaufen_sich_ihre_gesamten_Einkaufskosten?" = InOut.readDouble("Auftragswert="Wie_hoch_belaufen_sich_ihre_gesamten_Einkaufskosten?": ");
+    if (Auftragswert>Preis) {
+      Rabatt=Auftragswert*Rabatt;
+      Auftragswert=Auftragswert-Rabatt;
+      Ausgabe="Ihr Preis beträgt: " + Auftragswert;
+    } else {
+      Auftragswert= Auftragswert + Versandpauschale;
+      Ausgabe="Ihr Preis beträgt: " + Auftragswert;
+    }
+   
+  } // end of main
+
+} // end of class OnlineKaufhaus
+
